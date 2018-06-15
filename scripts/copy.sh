5994b6cf5e38a54d9fb46b031449a0e9d4afd4cf
Marcos.Cela <mcela@gradiant.org>
2018-06-15 10:02:56 +0200

    Changed copy.sh to use /bin/sh

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 echo "Copying JRE to volume..."
-rm -rf /shared/*
 cp -R /shared-origin/. /shared/
-echo "Setting permission to bamboo user!"
-chown -R bamboo:bamboo /shared/
 echo "Correctly injected dependencies to /shared"
